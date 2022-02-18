12f899479afa682cdf2ebcb0cc3599d5589d4117
ali eidelkhani <aliidelkhani18@gmail.com>
2022-02-19 01:24:28 +0330

    first commit

---
+++ src/user/user.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { LoggerService } from 'src/logger/logger.service';
+import { UserRepository } from 'src/repositories/user-repository.repository';
+import { UtilityService } from 'src/utility/utility.service';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UserEntity } from './entities/user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    private readonly loggerService: LoggerService,
+    @InjectRepository(UserEntity)
+    private readonly userRepository: UserRepository,
+    private readonly utilityService: UtilityService,
+  ) {
+    this.loggerService.setPrefix('UserService');
+    this.loggerService.log('Constructor called');
+  }
+
+  findAll() {
+    return this.userRepository.find();
+  }
+
+  create(data: CreateUserDto) {
+    data.password = this.utilityService.hash(data.password);
+    const user = this.userRepository.create(data);
+    return this.userRepository.save(user);
+  }
+}
